0b627d642adfa52b395b997ba0c126aabb53b1cd
Betina Kutsarova <betina.kutsarova@intellias.com>
2025-02-19 17:08:36 +0200

    add state management for login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,38 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
 
 function Login() {
+
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    function handleChange(e) {
+        const { name, value } = e.target
+        setFormData(prev => ({
+          ...prev,
+          [name]: value
+        }))
+      }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log('submitted login', formData)
+    }
+
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="email">Email</label>
-                <input type="email" id="emailid" name="email"/>
+                <input type="email" id="emailid" name="email" value={formData.email} onChange={handleChange} required/>
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="passwordid" name="password"/>
+                <input type="password" id="passwordid" name="password" value={formData.password} onChange={handleChange} required/>
             </div>
             <button type="submit">Login</button>
             <p> Dont have an account? <Link to="/register">Register here</Link></p>
