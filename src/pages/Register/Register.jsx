3125cc9b6da6744e82a7671c5f3a8c7b4ca3139c
Betina Kutsarova <betina.kutsarova@intellias.com>
2025-02-19 16:34:53 +0200

    add static register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Register.css'
 
 
@@ -7,6 +7,26 @@ function Register() {
     return (
       <div>
         <h1>Register</h1>
+        <form>
+            <div>
+                <label htmlFor="username">Username</label>
+                <input type="username" id="usernameid" name="username"/>
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="emailid" name="email"/>
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="passwordid" name="password"/>
+            </div>
+            <div>
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input type="password" id="confirmPasswordid" name="confirmPassword"/>
+            </div>
+            <button type="submit">Register</button>
+            <p> Already have an account? <Link to="/login">Login here</Link></p>
+        </form>
       </div>
     )
   }
