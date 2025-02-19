0d10b8fd163144728760e1fe49f76eb778230b07
Betina Kutsarova <betina.kutsarova@intellias.com>
2025-02-19 16:25:14 +0200

    add static login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,23 @@
 // import { useState } from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Login.css'
 
 function Login() {
     return (
       <div>
         <h1>Login</h1>
+        <form>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="emailid" name="email"/>
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="passwordid" name="password"/>
+            </div>
+            <button type="submit">Login</button>
+            <p> Dont have an account? <Link to="/register">Register here</Link></p>
+        </form>
       </div>
     )
   }
